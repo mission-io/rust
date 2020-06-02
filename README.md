99337d9fe92885e00cc1ed55780f4e6863990fb3
Natarajan Ganapathi <natarajanmca11@gmail.com>
2020-06-02 15:43:29 +0530

    clean-up

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# rust
+# Mission in RUST for REST
 REST Api using RUST
+
+
+
+
+
+## Debug Rust in VS Code
+1. Install VS Code Plugin "CodeLLDB" for native code debugging.
+
+
+
+### Reference
+[How to debug Rust](https://www.forrestthewoods.com/blog/how-to-debug-rust-with-visual-studio-code/)
+
+rustup component add rust-analysis --toolchain nightly-x86_64-apple-darwin
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
-
 extern crate rocket;
 mod routes;
 
@@ -8,14 +7,16 @@ use rocket::response::content;
 
 #[catch(404)]
 fn not_found(req: &rocket::Request<'_>) -> content::Html<String> {
-    content::Html(format!("<p>Sorry, but '{}' is not a valid path!</p>
+    content::Html(format!(
+        "<p>Sorry, but '{}' is not a valid path!</p>
             <p>Try visiting /hello/&lt;name&gt;/&lt;age&gt; instead.</p>",
-            req.uri()))
+        req.uri()
+    ))
 }
 
 fn main() {
     rocket::ignite()
-    .mount("/hello", routes![routes::user::hello, routes::user::world])
-    .register(catchers![not_found])
-    .launch();
+        .mount("/hello", routes![routes::user::hello, routes::user::world])
+        .register(catchers![not_found])
+        .launch();
 }
